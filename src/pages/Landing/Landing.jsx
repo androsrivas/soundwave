9dc9508f4b2e810853d9e9701d36383a8fe3af8b
dustisme <androsrivas@outlook.com>
2024-11-05 15:55:47 +0100

    add background circles to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,8 +1,13 @@
 import MainLanding from "../../components/organisms/MainLanding/MainLanding";
+import Circle from "../../components/atoms/Circle/Circle";
+import "./Landing.css";
 
 function Landing() {
   return (
     <>
+      <Circle className="circle1"/>
+      <Circle className="circle2"/>
+      <Circle className="circle3"/>
       <MainLanding />
     </>
   )
