62db911950331a4bbb84d70206c7f05fec99cee7
dustisme <androsrivas@outlook.com>
2024-11-05 16:01:00 +0100

    add background circles to join page

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,9 +1,13 @@
 import MainJoin from "../../components/organisms/MainJoin/MainJoin";
 import Footer from "../../components/molecules/Footer/Footer";
+import Circle from "../../components/atoms/Circle/Circle";
+import "./Join.css";
 
 const Join = () => {
   return (
     <>
+        <Circle className="circle1"/>
+        <Circle className="circle2"/>
         <MainJoin />
         <Footer />
     </>
