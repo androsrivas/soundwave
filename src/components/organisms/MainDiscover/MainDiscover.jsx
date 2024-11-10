76510394ce44d99cbfbd64eda82a9a9cb5bfcdc4
dustisme <androsrivas@outlook.com>
2024-11-10 17:05:33 +0100

    this is the finale coooommiiiit. ninoninooooo ninoninoniiiii

--- src/components/organisms/MainDiscover/MainDiscover.jsx
+++ src/components/organisms/MainDiscover/MainDiscover.jsx
@@ -4,15 +4,13 @@ import "./MainDiscover.css";
 
 function MainDiscover() {
   return (
-    <div className="mainContainer">
-      <section className="claim">
+    <div className="discover-main-container">
+      <section className="discover-claim">
         <h2>Discover new music</h2>
         <IconSection />
         <p>By joining you can benefit by listening to the latest albums released.</p>
       </section>
-      <section className="image">
         <img src={ pic } alt="Covers" className="covers"></img>
-      </section>
     </div>
   )
 }
